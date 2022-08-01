106bf1cfb8b66765cd240a1d0ae8cea3d8c134e9
davidcode10 <davidedson2030@outlook.com>
2022-08-01 16:27:01 +0200

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by David Santiago on 31/7/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by David Santiago on 31/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationTitle("H4K0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Hola"),
+//    Post(id: "3", title: "Oi"),
+//    Post(id: "4", title: "Buna")
+//]
